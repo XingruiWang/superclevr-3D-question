cbbf7779330817bdba27a78b8d1f540c388fbf28
Lizw14 <lizhuowan14@gmail.com>
2022-03-29 13:56:03 -0400

    align redundancy problem

--- scripts/generate_no_redundant.sh
+++ scripts/generate_no_redundant.sh
@@ -9,37 +9,38 @@ cd question_generation
 #     --num_scenes 10
 
 # Generate questions with part template for super-CLEVR
-START_IDX=25000
+START_IDX=0
 
 python generate_questions.py \
-   --input_scene_file ../output/ver_mask/superCLEVR_scenes.json \
+   --input_scene_file ../output/superCLEVR_scenes_100mb.json \
    --scene_start_idx ${START_IDX} \
-   --num_scenes 5000 \
+   --num_scenes 5 \
    --instances_per_template 1 \
    --templates_per_image 10 \
    --remove_redundant 1.0 \
    --metadata_file metadata_part.json \
    --template_dir CLEVR_1.0_templates \
-   --output_questions_file ../output/ver_mask/no_redundant/superCLEVR_questions_nrd_${START_IDX}.json
-#
-# Align to the non-redundant examples 
-# python align_removed.py \
-#    --kept-file ../output/tmp/no_redundant/superCLEVR_questions_rd_old_${START_IDX}.json \
-#    --new-file ../output/tmp/no_redundant/superCLEVR_questions_nrd_old_${START_IDX}.json \
-#    --out-file ../output/tmp/no_redundant/superCLEVR_questions_aligned_old_${START_IDX}.json
-#
-# echo "wrote output to ../output/superCLEVR_questions_part_aligned.json"
+   --output_questions_file ../output/align/superCLEVR_questions_nrd_${START_IDX}.json
 
 # Generate questions with original CLEVR template for super-CLEVR
-# python generate_questions.py \
-#     --input_scene_file ../output/superCLEVR_scenes_5.json \
-#     --scene_start_idx 0 \
-#     --num_scenes 5 \
-#     --instances_per_template 1 \
-#     --templates_per_image 100 \
-#     --metadata_file metadata_part.json \
-#     --output_questions_file ../output/tmp/superCLEVR_questions_30000.json \
-#     --template_dir CLEVR_1.0_templates
+python generate_questions.py \
+    --input_scene_file ../output/superCLEVR_scenes_100mb.json \
+    --scene_start_idx 0 \
+    --num_scenes 5 \
+    --instances_per_template 1 \
+    --templates_per_image 10 \
+    --metadata_file metadata_part.json \
+    --output_questions_file ../output/align/superCLEVR_questions_${START_IDX}.json \
+    --template_dir CLEVR_1.0_templates
+
+#
+# Align to the non-redundant examples 
+python align_removed.py \
+   --kept-file ../output/align/superCLEVR_questions_${START_IDX}.json \
+   --new-file ../output/align/superCLEVR_questions_nrd_${START_IDX}.json \
+   --out-file ../output/align/superCLEVR_questions_aligned_${START_IDX}.json
+
+echo "wrote output to ../output/superCLEVR_questions_part_aligned.json"
 
 cd -
 
